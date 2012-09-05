d0ec86ce46ee58f4464d9321757b0b1415757b19
unknown <Steven@Nope.(none)>
2012-09-05 14:44:39 +0200

    Environment added

---
+++ source/environment.py
@@ -0,0 +1,56 @@
+# Autonomous Agents - 2012/2013
+# Team SuperAuke
+#
+# playingField.py
+# Base Class for the playingfield
+import predator
+import prey
+
+class Environment:
+    
+    '''
+    Creates an instance of the environment. Default an eleven by eleven grid
+    is used. The default position for the prey is (5,5).
+    '''
+    
+    def __init__( self, width = 11, height = 11, preyLocation = (5,5) ):
+        self.width  = width
+        self.height = height
+        self.predators = []
+        self.prey = prey.Prey( preyLocation )
+    
+    def getWidth( self ):
+        '''Returns the width of the environment.'''
+        return self.width
+    
+    def getHeight( self ):
+        '''Returns the height of the environment.'''
+        return self.height
+    
+    def addPredator( self, location ):
+        '''Adds a predator to the environment at the given location.'''
+        newPredator = predator.Predator( location )
+        self.predators.append( newPredator )
+    
+    def getState( self ):
+        '''Returns the current environment state.'''
+        # Create state
+        state = dict()
+        state['predator'] = []
+        state['prey'] = prey.getLocation
+        
+        # Retrieve predator positions
+        for predator in self.predators:
+            state['predator'].append( predator.getLocation )        
+    
+    def run( self ):
+        '''Performs one step of the simulation.'''
+        # Retrieve the state
+        state = self.getState()
+        
+        # Update predator positions
+        for predator in self.predators:
+            predator.move( self.width, self.height, state )
+            
+        # Update prey position
+        self.prey.move( self.width, self.height, state )
