57fd644902ac6f65fbbf6cf09448b8000cbbafb9
Steven Laan <TheRealNeojume@gmail.com>
2012-09-17 18:05:56 +0200

    Value Iteration + Policy Evaluation

--- source/value_iteration.py
+++ source/value_iteration.py
@@ -1,92 +1,127 @@
-# Autonomous Agents - 2012/2013
-# Team SuperAuke
-#
-# valueIteration.py
-# Base class for the prey
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 15 15:13:54 2012
 
-import copy
+@author: Steven
+"""
 
-def valueIteration():
-    
-    # Define the set of actions
-    actions = set( [ (0,0),(1,0), (0,1), (-1,0), (0,-1) ] )
-              
-    # Define the discount
-    discount = 0.8
+size = 11
+discount = 0.7
 
-    # Define the set of all states
-    S = set( [ (i,j,k,m) for i in range(11) for j in range(11) for k in range(11) for m in range(11) ] )
+theta = 0.0001
+delta = 1
+
+S = set()
+V = dict()
+new_V = dict()
+actions = set( [ (0,0), (1,0), (0,1), (-1,0), (0,-1) ] )
+terminal_states = set()
+
+def performMove( state, action, prey ):  
+    if prey:
+        x,y = state[0],state[1]
+    else:
+        x,y = state[2],state[3]
     
-    # Define delta and theta
-    delta = 0.2   
-    theta = 0.01    
+    new_x = (x + action[0]) % size
+    new_y = (y + action[1]) % size
     
-    # Define dictionaries for the Value-function
-    V = dict()    
-    new_V = dict()    
-    
-    # The dictionary for the new policy
-    policy = dict()    
-    
-    # Initialize the Value function to zero
-    for s in S: 
-        V[s] = 0
+    if prey:
+        return ( new_x, new_y, state[2], state[3] )
+    else:
+        return ( state[0], state[1], new_x, new_y )
         
-        # loop over all action to fill the policy dictionary
-        for a in actions:
-            policy[(s,a)] = 0
+def preyMoves( state ):
+    next_states = dict()
     
-    # Policy iteration
-    while delta > theta:
-        delta = 0
+    if state in terminal_states:
+        next_states[state] = 1
+        return next_states
         
-        for s in S:
-            new_V[s] = 0
-            v = V[s]                        
-            
-            best_value = None
-            best_action = None
-            for a in actions:
-                current_value = 0
-                policy[(s,a)] = 0
-                
-                # Calculate all next states and their probabilities
-                next_states, P = nextStates( s, a )
-                
-                for next_state in next_states:
-                    current_value +=  P[next_state] * ( reward( next_state ) + discount * V[next_state] )
-            
-                if current_value > best_value:
-                    best_value = current_value
-                    best_action = a
-                    
-            policy[ ( s, best_action ) ] = 1.0            
-            new_V[s] = best_value
-            
-            # Compute the error
-            delta = max( delta, abs( v - new_V[s] ) )
-
-        # Store the new values
-        V = copy.deepcopy(new_V)
-        
-    return policy
-
-def reward( state ):
+    for a in actions:
+        new_state = performMove( state, a, True )
+        if not new_state in terminal_states:
+            next_states[new_state] = 1
     
+    p_move = 0.2 / ( len( next_states ) - 1 )   
+    #next_states[state] = 0.8
+    
+    for s in next_states:
+        if s == state:
+            next_states[s] = 0.8
+        else:
+            next_states[s] = p_move
+            
+    return next_states
+
+def nextStates( state, action ):
+    new_state = performMove( state, action, False )
+    return preyMoves( new_state )
+    #new_states = preyMoves( state )
+    #next_states = dict()    
+    #for new_state in new_states:
+    #    next_states[ performMove( new_state, action, False ) ] = new_states[new_state]
+    #return next_states
+    
+def reward( state ):        
     if (state[0],state[1]) == (state[2], state[3]):
         return 10
     return 0
 
-def nextStates( state, action ):
-    '''
-    Returns a tuple containing a list of all possible next states and a
-    dictionary containing the transition probabilities of those next states.
-    '''
-    old_x, old_y = (state[0], state[1])
-    
-    # determine the new location based on environment borders
-    new_x = (old_x + action[0]) % 11
-    new_y = (old_y + action[1]) % 11
-    
-    return [(new_x, new_y, state[2],state[3])], { (new_x, new_y, state[2],state[3]) : 1}
+# Initialize V and S
+for i in xrange( size ):
+    for j in xrange( size ):
+        for k in xrange( size ):
+            for m in xrange( size ):
+                s = (i,j,k,m)          
+                V[s] = 0                
+                if i == k and j == m:
+                    terminal_states.add( s )
+                else:
+                    S.add( s )
+                
+# Define the policy (currently random: 5 actions per state, each prob(a) = 0.2)
+policy = dict()
+for s in S:
+    for a in actions:
+        policy[(s,a)] = 0.2
+
+# Define delta and theta
+delta = 0.2   
+theta = 0.001    
+
+step = False
+view = False
+# Policy evaluation
+while delta > theta:
+    delta = 0
         
+    for s in S:
+        new_V[s] = 0                      
+        
+        best_value = None
+        best_action = None
+        
+        for a in actions:
+            current_value = 0
+            policy[ (s, a ) ] = 0
+            
+            # Calculate all next states and their probabilities
+            P = nextStates( s, a )
+            
+            for next_state in P:
+                current_value +=  P[next_state] * ( reward( next_state ) + discount * V[next_state] )
+        
+            if current_value > best_value:
+                best_value = current_value
+                best_action = a
+                
+        policy[ ( s, best_action ) ] = 1.0            
+        new_V[s] = best_value
+        
+        # Compute the error
+        delta = max( delta, abs( V[s] - new_V[s] ) )
+
+    # Store the new values
+    for s in S:
+        V[s] = new_V[s]
