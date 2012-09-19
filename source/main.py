bde416fa26177fd1ed8aa010a079ca2da52e937a
Camiel Verschoor <verschoor@uva.nl>
2012-09-19 20:55:05 +0200

    New main function calling gui

--- source/main.py
+++ source/main.py
@@ -1,5 +1,42 @@
-import environment
+from EnvironmentNormal import EnvironmentNormal
+from EnvironmentReduced import EnvironmentReduced
+from interface import interface
 
+def main():
+    ''' Main function running single agent planning and the GUI '''
+    # Initialize environment and get the begin state
+    E = EnvironmentNormal()
+    s = E.getState()
+
+    # Initialize Graphical User Interface with current state
+    GUI = Interface( (11, 11), s )
+    GUI.start()
+
+    # Decode the state
+    pred_x, pred_y, prey_x, prey_y = s
+
+    # If the prey is not caught then run.
+    while( not( (pred_x, pred_y) == (prey_x, prey_y) ) ):
+        # Run a step
+        E.run()
+        s = E.getState()
+
+        pred_x, pred_y, prey_x, prey_y = s
+
+        # Set new locations in GUI
+        GUI.setPredator( (pred_x, pred_y) )
+        GUI.setPrey( (prey_x, prey_y) )
+
+    # Wait for exit event in GUI.
+    GUI.join()
+
+# If this is the main script run main function
+if( __name__ == '__main__' ):
+    main()
+
+
+'''
+Old shit!!!!!!!!!!!!!!!
 # Counter for number of iterations
 total = 0
 N = 100
@@ -21,4 +58,4 @@ for i in xrange( N ):
     total += iterations
 
 print 'Averge number of iterations:', total / float( N )
-    
+''' 
