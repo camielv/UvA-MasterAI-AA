ff6f10bf311f0657d631cd2c82d5f0b395b20c19
Aukejw <wiggers.auke@gmail.com>
2012-10-10 15:53:18 +0200

    Update environment

--- source/MultiAgent/Environment.py
+++ source/MultiAgent/Environment.py
@@ -1,4 +1,4 @@
-# Assignment:   Single Agent Learning
+# Assignment:   MultiAgent Planning&Learning
 # Course:       Autonomous Agents 2012-2013
 # Education:    Master Artificial Intelligence
 # By:           Steven Laan
@@ -11,6 +11,7 @@ from Predator import Predator
 from Prey import Prey
 
 import time
+import random
 
 class Environment:
     '''
@@ -31,200 +32,136 @@ class Environment:
         self.width  = width
         self.height = height
         self.S,self.terminal_states = self.getStates()
+        self.numberOfPredators = numberOfPredators
 
         self.Prey = Prey( self, preyLocation )
         self.PredatorLocations = [(0,0), (10,0), (0,10), (10,10)]
 
         self.Predators = [Predator(self, self.predatorLocations[i]) \
-                          for i in range(numberOfPredators)]        
-    
-                        
-                
-    def getState( self ):
-        '''Returns the current environment state.'''        
-        raise NotImplementedError
+                          for i in range(self.numberOfPredators)]        
+
+           
+    def environmentStep(self):
+        '''
+        s_prime = environmentStep()
         
+        Perform one step (for the prey and each predator), given the current 
+        state s.
+        '''
+        
+    def qLearning( self, 
+           episodes=1000, 
+           alpha=0.1, 
+           gamma=0.1, 
+           epsilon=0.1, 
+           optimistic_init=15,
+           verbose=False):
+        '''
+        Q, return_list <- qLearning(episodes, 
+                                    alpha, 
+                                    gamma, 
+                                    epsilon,
+                                    optimistic_init)
+        
+        Implementation of Q-learning given the learning rate alpha, discount
+        factor gamma, epsilon for epsilon-greedy policy or temperature tau for
+        softmax action selection. Integer optimistic_init is used 
+        to initialize Q, and should be larger than zero.
+
+        Note that if epsilongreedy == False, epsilon will be used as the 
+        temperature tau.
+        
+        Returns the values of Q as a 2-deep dict (Q[s][a]) and a list
+        containing performance measures of the agent (number of steps).
+        '''
+        
+        now = time.time()            
+        # For a number of episodes
+        for n in xrange(1,episodes+1):
+            
+            if verbose and n % 50 == 0:
+                print 'Episode {0}, time taken: {1}.'.format(n, time.time()-now)
+                now = time.time()
+                
+            # Initialize the beginstate s semi-randomly
+            s = [(0,0)] + [(random.randint(-5,5), random.randint(-5,5)) \
+                           for i in range(self.numberOfPredators)]
+
+            game_over = False
+            step_number = 0           
+            
+            # Run through one episode
+            while not game_over:      
+                step_number += 1
+        
+                # All agents take one QLearning step simultaneously, but we
+                # calculate the state that results from the prey's new position
+                actions = list()                
+                for Agent in self.Agents:                    
+                    # Find a sample action (epsilon-greedy) given the state
+                    a = self.Agent.deriveAction(s)
+                    actions.append(a)
+                    
+                    # Perform that action
+                    Agent.performAction(Agent.Q, s, Agent.epsilon)
+                    
+                # Derive the new state from updated agent locations
+                s_prime = self.deriveState([Agent.location for Agent in \
+                                            self.Agents])                                
+                
+                # This results in reward
+                r, game_over = self.reward(s_prime)
+                
+                # Update Q for each agent
+                for Agent in self.Agents:
+                    Agent.QLearning.updateQ( s, a, s_prime, r )
+                
+                s = s_prime
+           
     def getStates(self):
         '''
         Gets the entire statespace in two sets, the first containing the non-
         terminal states and the second containing terminal states.
         '''
-        raise NotImplementedError
-
-    def reward( self, s, a, s_prime ):
-        '''
-        Returns the reward for a given future 4d state containing location of 
-        the predator and prey. In this case, state and action are actually 
-        redundant but are added for the sake of completeness. 
-        '''
-        raise NotImplementedError
-
-    def nextStates( self, s, a ):
-        raise NotImplementedError
-          
-    def policyEvaluation( self, gamma = 0.8 ):
-        '''
-        Performs policyEvaluation for the given predator in this environment. 
-        '''
-        
-        V = dict()
-        # For every state in the statespace including terminal states
-        for s in self.S | self.terminal_states:
-            V[s] = 0
-          
-        # Get the current policy 
-        policy = self.Predator.policy
-
-        # Define delta and theta
-        delta = 0.2
-        theta = 0   
-        
-        # Keep track of the number of iterations
-        i = 0
-        
-        # Policy evaluation
-        while delta > theta:
-            delta = 0
-            new_V = dict()
-           
-            for s in self.S:
-                new_V[s] = 0               
-               
-                for a in self.Predator.actions:
-                    # Calculate all next states and their probabilities
-                    P = self.nextStates( s, a )
-                    for s_prime in P:
-                        new_V[s] += policy[(s,a)] * P[s_prime] * ( self.reward( s, a, s_prime ) + gamma * V[s_prime] )
-                        
-                # Compute the error
-                delta = max( delta, abs( V[s] - new_V[s] ) )
-         
-            # Store the new values
-            V.update( new_V )
-            
-            i += 1
-
-        print 'Policy Evaluation took', i, 'iterations'
-        return V
-
-    def valueIteration( self, gamma = 0.7 ):    
-        ''' 
-        Perform value iteration for this environment. 
-        '''        
-        now = time.time()
-        
-        iterations = 0        
-        
-        V = dict()
-        for s in self.S | self.terminal_states:
-            V[s] = 0
-        
-        # Define delta and theta
-        delta = 0.2  
-        theta = 0.0
-
-        new_V = dict()
-            
-        # Policy evaluation
-        while delta > theta:
-            iterations += 1
-            delta = 0
-            for s in self.S:
-                new_V[s] = 0                     
-                
-                best_value = None
-                best_action = None
-                
-                for a in self.Predator.actions:
-                    current_value = 0
+        S = set()
+        terminal_states = set()
+    
+        for i in xrange( -(self.width/2), self.width/2+1 ):
+            for j in xrange( -(self.height/2), self.height/2+1 ):
+                s = list() 
+                for p in xrange(self.numberOfPredators):
+                    s.append( (i,j) )
+                # If absorbing (reached predator/two predators at one spot)
+                if (0,0) in s or len(s) != len(set(s)):
+                    terminal_states.add( s )
+                else:
+                    S.add( s )
                     
-                    # Calculate all next states and their probabilities
-                    P = self.nextStates( s, a )
-                    
-                    # Find the action that maximizes the value
-                    for s_prime in P:
-                        current_value +=  P[s_prime] * ( self.reward( s, a, s_prime ) + gamma * V[s_prime] )
-                
-                    if current_value > best_value:
-                        best_value = current_value
-                        best_action = a
-                        
-                # In the new policy, that action is now the only one that is taken
-                self.Predator.updatePolicy( s, best_action )
-                new_V[s] = best_value
-                
-                # Compute the error
-                delta = max( delta, abs( V[s] - new_V[s] ) )
-        
-            # Store the new values
-            V.update( new_V )
-        print 'Number of iterations for discount', gamma, ' :', iterations
-        print 'Time taken (seconds): ', time.time() - now
-        return V        
-        
-    def policyIteration( self, gamma=0.7 ):
+        return S, terminal_states    
+    
+    def reward( self, s ):
         '''
-        Performs policy iteration starting from the policy of the predator.
-        '''
-        now = time.time()
-        
-        # Initialization
-        stable = False
-                
-        while not stable:
-            # 1. Policy evaluation
-            V = self.policyEvaluation( gamma )
-            
-            # 2. Policy Improvement
-            stable = self.policyImprovement( V, gamma )
+        r, game_over <- reward(s)
 
-        print 'Time taken (seconds): ', time.time() - now
-        return V
-
-    def policyImprovement( self, V, gamma=0.7 ):
+        Returns the reward for a given state containing location of 
+        the predators and prey. Return is the reward of the predator(s), as 
+        this is a zero-sum-game, the reward of the predator is simply the 
+        negated reward of the prey. Boolean game_over indicates if the state 
+        is absorbing.
         '''
-        Performs policy improvement, given the value function V.        
-        '''
-        
-        updated = 0
-        stable = True
-        
-        for s in self.S:
-            # Retrieve action according to policy
-            policy_action = self.Predator.getAction( s )
-            
-            best_action = None
-            best_value = None
-            
-            # Check all possible actions
-            for a in self.Predator.actions:                
-                current_value = 0
-                
-                # Calculate next states and their probabilities
-                P = self.nextStates( s, a )
-                
-                for s_prime in P:
-                    current_value += P[s_prime] * ( self.reward( s, a, s_prime ) + gamma * V[s_prime] )
-                    
-                if current_value > best_value:
-                    best_value = current_value                    
-                    best_action = a                    
+        # Prioritize confusion of predators            
+        if len(s) != len(set(s)):
+            return -10, True
+        elif (0,0) in s:
+            return 10, True
+        else:
+            return 0, False
 
-            # Update the policy
-            self.Predator.updatePolicy( s, best_action )
-            
-            # Check policy stability
-            if best_action != policy_action:
-                updated += 1
-                stable = False
-            
-        print 'Updated', updated, 'actions'
-        return stable
-        
     def reset(self):
         '''
         Reset the position of the prey and predator in this environment.        
         '''
         self.Prey.location = (5,5)
-        self.Predator.location = (0,0)
-        
+        self.Predators = [Predator(self, self.predatorLocations[i]) \
+                          for i in range(self.numberOfPredators)]        
+    
