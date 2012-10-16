74ba9fab941fdb12d6a0fb710e1cfacc78b7143a
Aukejw <wiggers.auke@gmail.com>
2012-10-16 15:11:45 +0200

    Revert "Qlearning now working for 2 agents, 1 predator. Due to statespace, more predators result in memoryerrors"
    This reverts commit 519a433674da5c26a20e7c502d1ce2bee3b7023e.

--- source/MultiAgent/Predator.py
+++ source/MultiAgent/Predator.py
@@ -15,27 +15,7 @@ class Predator( Agent ):
     Creates an agent which contains a policy. Input are an environment object
     and the location of the agent, which is (0,0) by default.    
     '''
-
-    def performAction( self, a ):
-        ''' 
-        performAction( a )        
-        
-        Update the location of an agent based on a given action a.
-        '''
-        old_x, old_y = self.location
-
-        new_x = (old_x + a[0]) % self.Environment.width
-        new_y = (old_y + a[1]) % self.Environment.height
-        
-        self.location = (new_x, new_y)
-
     
-    def updateQ( self, s, a, s_prime, r ):
-        '''
-        updateQ( s, a, s_prime, r )
-
-        Given the old state s, an action this agent took, the new state s_prime
-        and the reward for going to this new state, update Q.
-        '''
+    def updateQ(self, s, a, s_prime, r):
         self.QLearning.updateQ( s, a, s_prime, r )
     
